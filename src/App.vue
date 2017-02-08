eb1cd982e37c0b4eeff44d5acab3aeada2eaa427
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2017-02-08 01:24:26 -0800

    Adding kube and some global styles

--- src/App.vue
+++ src/App.vue
@@ -12,28 +12,4 @@ export default {
 }
 </script>
 
-<style>
-* {
-  box-sizing: border-box;
-}
-
-html, body {
-  margin: 0;
-}
-
-body {
-  font-family: sans-serif;
-}
-
-a {
-  text-decoration: none;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./scss/global.scss" lang="scss"></style>
